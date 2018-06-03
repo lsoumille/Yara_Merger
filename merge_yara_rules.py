f72ff7247480bf0c949a485e3065622509ab2e2c
lsoumille <lucas.soumille@etu.unice.fr>
2018-06-03 11:08:19 +0200

    need to be tested

--- merge_yara_rules.py
+++ merge_yara_rules.py
@@ -2,24 +2,25 @@
 # encoding: utf-8
 
 import os
+import shutil
+import git
 
-#Get all folder nanes and remove Android YARA rules
-def get_all_yara_folder(folder):
-	all_in_dir = os.listdir(folder)
-	all_yara_dirs = []
-	for dir_file in all_in_dir:
-		if not (("." in dir_file) or (dir_file == "Mobile_Malware") or (dir_file == "LICENSE")):
-			all_yara_dirs.append(folder + "/" + dir_file)
-	return all_yara_dirs
-
+#Delete official repo and clone it to retrieve last yara rules
+def update_yara_rule_folder(folder):
+	shutil.rmtree(folder)
+	git.Git().clone("https://github.com/Yara-Rules/rules.git")
 
 #Get all Yara files
-def get_yara_files(folders):
+def get_yara_files(folder):
 	all_yara_files = []
-	for yara_dir in folders:
-		all_yara_files_in_dir = os.listdir(yara_dir)
-		for yara_file in all_yara_files_in_dir:
-			all_yara_files.append(yara_dir + "/" + yara_file)
+	for root, directories, filenames in os.walk(folder):
+		if root == folder or root == "Mobile_Malware":
+			continue
+		for file_name in filenames:
+			if file_name == "MALW_TinyShell_Backdoor_gen.yar":
+				continue
+			if ".yar" in file_name:
+				all_yara_files.append(os.path.join(root, file_name))
 	return all_yara_files
 
 #Filter Yara files with import math, imphash function and is__osx rule TODO
@@ -28,7 +29,7 @@ def remove_incompatible_imports(files):
 	for yara_file in files:
 		with open(yara_file, 'r') as fd:
 			yara_in_file = fd.read()
-			if not (("import \"math\"" in yara_in_file) or ("imphash" in yara_in_file)):
+			if not (("import \"math\"" in yara_in_file) or ("import \"hash\"" in yara_in_file) or ("imphash" in yara_in_file)):
 				yara_files_filtered.append(yara_file)
 	return yara_files_filtered
 
@@ -52,13 +53,17 @@ def remove_iself_duplicates(files):
 					to_delete = False
 	return yara_files_filtered
 
+def dump_in_file(all_rules):
+	with open("all_yara_rules.yar", 'w') as fd:
+		fd.write(''.join(all_rules))
+
 def main():
-	root_yara = "rules_test"
-	all_folders = get_all_yara_folder(root_yara)
-	all_yara_files = get_yara_files(all_folders)
+	root_yara = "rules"
+	update_yara_rule_folder(root_yara)
+	all_yara_files = get_yara_files(root_yara)
 	all_yara_filered_1 = remove_incompatible_imports(all_yara_files)
 	all_yara_filered_2 = remove_iself_duplicates(all_yara_filered_1)
-	print(''.join(all_yara_filered_2))
+	dump_in_file(all_yara_filered_2)
 
 # Main body
 if __name__ == '__main__':
